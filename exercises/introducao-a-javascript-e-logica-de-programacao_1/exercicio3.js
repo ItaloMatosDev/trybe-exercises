27dcd87838c6011c92bfea5cf60f16455556a745
Italo Matos <italomatos764@gmail.com>
2021-02-26 12:48:10 -0400

    Adicionado exercicio 3

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio3.js
@@ -0,0 +1,13 @@
+let numberA = 5;
+let numberB = 6;
+let numberC = 7;
+
+if(numberA > numberB && numberA>numberC){
+    console.log(`Maior: ${numberA}`);
+}
+else if(numberB>numberA && numberB>numberC){
+    console.log(`Maior: ${numberB}`);
+}
+else{
+    console.log(`Maior: ${numberC}`);
+}
