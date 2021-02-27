9008537c7f902e45d112f34c2f2ace75b2654ff3
Italo Matos <italomatos764@gmail.com>
2021-02-26 23:16:37 -0400

    somando todos os valores de um array

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio1.js
@@ -1,5 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let i;
 
-for(let i=0 ; i<numbers.length; i++){
+for(i=0 ; i<numbers.length; i++){
     console.log(numbers[i]);
 }
+
+let contador=0;
+for(i=0 ; i<numbers.length; i++){
+    contador = contador + numbers[i];
+}
+console.log("Soma: ", contador);
