edde3533a6804b3aadaae14263146e40b7c49896
Italo Matos <italomatos764@gmail.com>
2021-02-26 23:18:40 -0400

    Imprimindo a media

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio1.js
@@ -1,12 +1,17 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let i;
 
+//print the values
 for(i=0 ; i<numbers.length; i++){
     console.log(numbers[i]);
 }
 
+//print the sum of itens
 let contador=0;
 for(i=0 ; i<numbers.length; i++){
     contador = contador + numbers[i];
 }
 console.log("Soma: ", contador);
+
+//print average
+console.log(contador/numbers.length);
